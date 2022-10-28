d423e15297c1ab9342d7e87aa95a54491ff81e0b
Nitin <nitinkumar714147@gmail.com>
2022-10-28 22:16:11 +0530

    1st Commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import KanbanAPI from "./api/KanbanAPI.js"
+import Kanban from "./view/Kanban.js"
+
+new Kanban(
+    document.querySelector(".kanban")
+)
+
+//console.log(KanbanAPI.getItems(1))
+
+
